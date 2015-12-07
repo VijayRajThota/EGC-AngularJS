e43497811fcf89a41bbaba3526ea501e4f317cab
VijayRajThota <vijayrajthota@gmail.com>
2015-12-06 16:55:32 -0800

    test this

--- app/scripts/controller.js
+++ app/scripts/controller.js
@@ -71,7 +71,7 @@ $scope.confirmSomething = function() {
     }
     else{*/
       
-    $scope.master = angular.copy(user);
+    $scope.master = angular.copy(signup);
     $scope.signUpResult=alterDollar.signingUp({name:signup.name, username:signup.username, email:signup.email, password:signup.password, phone:signup.phone});
     $scope.signUpResult.$promise.then(function(data) {
           if(data.message=="success"){
